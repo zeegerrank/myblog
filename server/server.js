962bed574decc3a28126af0e2e25666c0c268617
zeegerrank <zeegerrank@gmail.com>
2023-09-20 21:08:11 +0700

    connect to database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
-require("dotenv").config();
-const express = require("express");
+require("dotenv").config();const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = mongoose;
 
 app.post("/", (req, res) => {
   return res.status(200).send("Hello World");
 });
 
-const PORT = process.env.PORT || 3500;
-app.listen(PORT, () => {
-  console.log(`Server runs on port ${PORT}`);
+const DB_SECRET = process.env.DB_SECRET;
+const COLLECTION_NAME = "main";
+
+db.connect(
+  `mongodb+srv://zeegerrank:${DB_SECRET}@cluster0.itngpio.mongodb.net/${COLLECTION_NAME}?retryWrites=true&w=majority`
+);
+db.connection.once("open", () => {
+  console.log("Database is connected");
+  const PORT = process.env.PORT || 3500;
+  app.listen(PORT, () => {
+    console.log(`Server runs on port ${PORT}`);
+  });
+});
+db.connection.on("error", (err) => {
+  console.log("db connect error: ", err);
 });
